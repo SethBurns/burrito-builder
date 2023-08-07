8788b7d95e119600bf9d863dcb0fb0f7d0719ed9
Seth Burns <seth@sethburns.com>
2023-08-07 09:28:50 -0700

    Install Cypress and get DOM rendering

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,39 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import './OrderForm.css';
+import { postOrder } from '../../apiCalls';
 
-function OrderForm(props) {
-  const [name, setName] = useState("");
+function OrderForm({ setOrders, orders }) {
+  const [name, setName] = useState('');
   const [ingredients, setIngredients] = useState([]);
+  const [formFilled, setFormFilled] = useState(false);
+  const [formError, setFormError] = useState(false);
+
+  useEffect(() => {
+    if (name.length && ingredients.length) {
+      setFormFilled(true);
+    } else {
+      setFormFilled(false);
+    }
+  }, [name, ingredients]);
+
+  useEffect(() => {
+    if (formError) {
+      setTimeout(() => {
+        setFormError(false);
+      }, 4000);
+    }
+  }, [formError]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    clearInputs();
+    if (formFilled) {
+      let order = {
+        name: name,
+        ingredients: ingredients,
+      };
+      postOrder(order)
+        .then((response) => {
+          setOrders([...orders, response]);
+          clearInputs();
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setFormError(true);
+    }
   }
 
   function clearInputs() {
-    setName("");
+    setName('');
     setIngredients([]);
-  };
+  }
+
+  function handleIngredientClick(e, ingredient) {
+    e.preventDefault();
+    setIngredients([...ingredients, ingredient]);
+    console.log(ingredient, ingredients);
+  }
 
   const possibleIngredients = [
-    "beans",
-    "steak",
-    "carnitas",
-    "sofritas",
-    "lettuce",
-    "queso fresco",
-    "pico de gallo",
-    "hot sauce",
-    "guacamole",
-    "jalapenos",
-    "cilantro",
-    "sour cream",
+    'beans',
+    'steak',
+    'carnitas',
+    'sofritas',
+    'lettuce',
+    'queso fresco',
+    'pico de gallo',
+    'hot sauce',
+    'guacamole',
+    'jalapenos',
+    'cilantro',
+    'sour cream',
   ];
   const ingredientButtons = possibleIngredients.map((ingredient) => {
     return (
       <button
         key={ingredient}
         name={ingredient}
-        // onClick={(e) => }
+        onClick={(e) => {
+          handleIngredientClick(e, ingredient);
+        }}
       >
         {ingredient}
       </button>
@@ -47,12 +88,16 @@ function OrderForm(props) {
         placeholder="Name"
         name="name"
         value={name}
-        // onChange={(e) => }
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
       />
 
       {ingredientButtons}
-
-      <p>Order: {ingredients.join(", ") || "Nothing selected"}</p>
+      {formError && (
+        <p className="error">You must fill out Name AND Ingredients.</p>
+      )}
+      <p>Order: {ingredients.join(', ') || 'Nothing selected'}</p>
 
       <button onClick={(e) => handleSubmit(e)}>Submit Order</button>
     </form>
