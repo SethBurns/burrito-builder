fad42cd0de03ef904e87a88b8cdc8bdbf6393adb
Seth Burns <seth@sethburns.com>
2023-08-07 10:42:08 -0700

    Finish cypress testing

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -25,5 +25,46 @@ describe("homepage spec", () => {
     possibleIngredients.forEach(ingredient => {
       cy.get(`button[name="${ingredient}"]`).should('contain', `${ingredient}`)
     })
+    cy.get('.order').eq(0).find('h3').should('contain', 'Pat')
+    cy.get('.order').eq(2).find('h3').should('contain', 'Alex')
   });
+  it('should allow a user to add their name and ingredients to an order, submit it, and see it on the DOM', () => {
+    cy.get('input[type="text"]').type('Seth')
+    cy.get('input[type="text"]').should('have.value', 'Seth')
+    cy.get('button[name="carnitas"]').click()
+    cy.get('button[name="pico de gallo"]').click()
+    cy.get('button[name="beans"]').click()
+    cy.get('button[name="queso fresco"]').click()
+    cy.get('form').find('p').should('contain', 'Order: carnitas, pico de gallo, beans, queso fresco')
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', (req) => {
+      req.body = {
+        name: "Seth",
+        ingredients: ['carnitas', 'pico de gallo', 'beans', 'queso fresco']
+      };
+      req.reply({
+        statusCode: 201,
+        fixture: 'postResponse'
+      })
+    }).as('postResponse')
+    cy.get('form').contains('button', 'Submit Order').click()
+    cy.wait('@postResponse').its('response.statusCode').should('eq', 201)
+    // I know waiting an arbitrary amount of time is insane, but my test fails without it even though I'm waiting for my stubbed response.
+    cy.wait(1000)
+    // If I don't wait, the div at index 3 isn't there yet. I can't figure out why.
+    cy.get('section').find('div').eq(3).find('h3').should('contain', 'Seth')
+  });
+  it('should not allow the submit button to be pressed unless a name and at least one ingredient is selected', () => {
+    cy.get('form').contains('button', 'Submit Order').click()
+    cy.get('.error').should('contain', 'You must fill out Name AND Ingredients.')
+    cy.get('section').find('div').last().find('h3').should('contain', 'Alex')
+    cy.get('input[type="text"]').type('Seth')
+    cy.get('form').contains('button', 'Submit Order').click()
+    cy.get('.error').should('contain', 'You must fill out Name AND Ingredients.')
+    cy.get('section').find('div').last().find('h3').should('contain', 'Alex')
+    cy.get('input[type="text"]').clear()
+    cy.get('button[name="cilantro"]').click()
+    cy.get('form').contains('button', 'Submit Order').click()
+    cy.get('.error').should('contain', 'You must fill out Name AND Ingredients.')
+    cy.get('section').find('div').last().find('h3').should('contain', 'Alex')
+  })
 });
